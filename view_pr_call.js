1953e058204fec3d2038ea5530e257ebf5027d84
TonyC <tonyfcohen@gmail.com>
2018-10-07 11:15:30 -0500

    Update view_pr_call.js

--- view_pr_call.js
+++ view_pr_call.js
@@ -63,17 +63,6 @@ window.call_view_pr = function(userPref) {
 	}
 
 	httpGetAsync();
+}
 
-
-
-// Modal Window
-
-
-
-// For Head - include jQuery and modals
-<!-- Remember to include jQuery :) -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
-
-<!-- jQuery Modal -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css" />
+window.call_view_pr('philanthropy');
